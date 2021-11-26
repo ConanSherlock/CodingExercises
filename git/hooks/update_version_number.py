28b6e0931bc63f9096c14634a33b418dbe68ccfe
Conan Sherlock <conan.sherlock@ucdconnect.ie>
2021-11-26 21:33:33 +0000

    Updating precommit hook

--- git/hooks/update_version_number.py
+++ git/hooks/update_version_number.py
@@ -1,22 +1,39 @@
 #!/bin/python
 # Python script to increment the version number in a version file
+import os
+from git import Repo
+
 
 def update_version_number(version_file_path, version_keyword):
-    version_file = open(version_file_path, 'r')
-    file_lines = version_file.readlines()
-    new_file_lines = []
+    ret_val = False
 
-    for line in file_lines:
-        if version_keyword in line:
-            new_build_number = int(line.split(version_keyword)[-1].strip("\n").strip("\r"))+1
-            line = "%s%s %d\n" % (line.split(version_keyword)[0], version_keyword, new_build_number)
-        new_file_lines.append(line)
+    try:
+        version_file = open(version_file_path, 'r')
+        file_lines = version_file.readlines()
+        new_file_lines = []
 
-    version_file.close()
+        repo = Repo(os.getcwd())
 
-    version_file = open(version_file_path, 'w+')
-    version_file.writelines(new_file_lines)
-    version_file.close()
+        for line in file_lines:
+            if version_keyword in line:
+                new_build_number = int(line.split(version_keyword)[-1].strip("\n").strip("\r"))+1
+                line = "%s%s %d\n" % (line.split(version_keyword)[0], version_keyword, new_build_number)
+            new_file_lines.append(line)
+
+        version_file.close()
+
+        version_file = open(version_file_path, 'w+')
+        version_file.writelines(new_file_lines)
+        version_file.close()
+
+        os.system("git add .")
+
+        ret_val = True
+
+    except Exception as e:
+        print(e)
+
+    return ret_val
 
 
 if __name__ == "__main__":
