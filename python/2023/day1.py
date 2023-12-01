1cef8d2c9fb0ec97c3ba26c9fb6d1552b5f58392
Conan Sherlock <conansherlock@live.ie>
2023-12-01 14:04:01 +0000

    testing hook

--- python/2023/day1.py
+++ python/2023/day1.py
@@ -23,6 +23,7 @@ class Day1:
         if type(input_string) != str:
             raise AoC2015Day1Exception("%s: %s" % (INVALID_TYPE_EXCEPTION, type(input_string)))
 
+
         self._current_calibration = -1
 
         found_digits = []
