de183610cfa2fdf3e7150548f9791dfa150484f0
Conan Sherlock <conansherlock@live.ie>
2023-12-01 14:02:51 +0000

    Adding initial version of day1 and git hooks

---
+++ python/2023/day1.py
@@ -0,0 +1,68 @@
+INVALID_TYPE_EXCEPTION = "Invalid data type given"
+
+
+class AoC2015Day1Exception(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+class Day1:
+    def __init__(self):
+        self._get_total_calibration = 0
+        self._current_calibration = -1
+
+    def reset(self):
+        self._get_total_calibration = 0
+        self._current_calibration = -1
+
+    def find_calibration(self, input_string):
+        # Checking for invalid input types
+        if type(input_string) != str:
+            raise AoC2015Day1Exception("%s: %s" % (INVALID_TYPE_EXCEPTION, type(input_string)))
+
+        self._current_calibration = -1
+
+        found_digits = []
+
+        for character in input_string:
+            try:
+                found_digits.append(int(character))
+            except ValueError:
+                # Ignore not a number
+                pass
+
+        if len(found_digits) > 1:
+            found_number = found_digits[0] * 10 + found_digits[-1]
+        elif len(found_digits) == 1:
+            found_number = found_digits[0] * 10 + found_digits[0]
+        else:
+            found_number = 0
+
+        self._current_calibration = found_number
+        self._get_total_calibration += self._current_calibration
+
+    def get_total_calibration(self):
+        return self._get_total_calibration
+
+    def get_current_calibration(self):
+        return self._current_calibration
+
+
+if __name__ == "__main__":
+
+    day1_file_path = "../../test/input_data/2023/day1_input.txt"
+
+    with open(day1_file_path, "r") as input_file:
+        input_data = input_file.readlines()
+
+    day1 = Day1()
+
+    for calibration_input in input_data:
+        day1.find_calibration(calibration_input)
+
+    print("--- Day 1: Trebuchet?! ---")
+    print("Part A Sum of all of the calibration values: %d" % day1.get_total_calibration())
+    print("Part B Sum of all of the calibration values: %d" % day1.get_basement_interation())
---
+++ test/unit_tests/python_tests/2023/day1_tests.py
@@ -0,0 +1,77 @@
+import importlib
+import unittest
+
+# This script should be run from PathToRep0/test/unit_tests/python_tests/2023
+# Try to general utils code
+try:
+    spec = importlib.util.spec_from_file_location("mod", "../../../../python/2023/day1.py")
+
+    day1 = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(day1)
+
+except FileNotFoundError as e:
+    print(e)
+    print("Failed importing AoC 2023 day1")
+    exit(-1)
+
+
+class AoC2015Day1Test(unittest.TestCase):
+    def setUp(self):
+        self.__class__.day1 = None
+
+    def test_given_inputs_calibration(self):
+        self.__class__.day1 = day1.Day1()
+
+        input_strings = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
+
+        expected_calibration_outputs = [12, 38, 15, 77]
+
+        expected_total_calibration = 142
+
+        expected_init_total_calibration = 0
+        expected_init_current_calibration = -1
+
+        self.assertEqual(expected_init_total_calibration, self.__class__.day1.get_total_calibration())
+        self.assertEqual(expected_init_current_calibration, self.__class__.day1.get_current_calibration())
+
+        for input_string, expected_calibration in zip(input_strings, expected_calibration_outputs):
+            self.__class__.day1.find_calibration(input_string)
+            actual_calibration = self.__class__.day1.get_current_calibration()
+            self.assertEqual(expected_calibration, actual_calibration)
+
+        self.assertEqual(expected_total_calibration, self.__class__.day1.get_total_calibration())
+
+        self.__class__.day1.reset()
+        self.assertEqual(expected_init_total_calibration, self.__class__.day1.get_total_calibration())
+        self.assertEqual(expected_init_current_calibration, self.__class__.day1.get_current_calibration())
+
+    def test_given_inputs_basement_iteration(self):
+        self.__class__.day1 = day1.Day1()
+
+        input_strings = [")", "()())", "("]
+
+        expected_basement_iteration_not_found = 0
+
+        expected_basement_outputs = [1, 5, expected_basement_iteration_not_found]
+
+        for input_string, expected_basement_output in zip(input_strings, expected_basement_outputs):
+            self.__class__.day1.reset()
+
+            self.assertEqual(expected_basement_iteration_not_found, self.__class__.day1.get_basement_interation())
+
+            self.__class__.day1.calc_floor(input_string)
+
+            actual_basement_value = self.__class__.day1.get_basement_interation()
+
+            self.assertEqual(expected_basement_output, actual_basement_value)
+
+    def test_bad_inputs(self):
+        self.__class__.day1 = day1.Day1()
+
+        expected_bad_data_exception = "Invalid data type given"
+
+        try:
+            self.__class__.day1.find_calibration(1)
+            self.assertTrue(False, "Exception not thrown")
+        except day1.AoC2015Day1Exception as excep:
+            self.assertTrue(expected_bad_data_exception in str(excep))
