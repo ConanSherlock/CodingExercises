c88b2680085035769084ca8bce1c383e6bc41403
Conan Sherlock <conansherlock@live.ie>
2023-12-01 16:53:35 +0000

    Day 1 complete

--- python/2023/year2023_day1.py
+++ python/2023/year2023_day1.py
@@ -1,3 +1,5 @@
+from typing import List, Dict
+
 INVALID_TYPE_EXCEPTION = "Invalid data type given"
 
 
@@ -9,6 +11,20 @@ class AoC2015Day1Exception(Exception):
         return repr(self.value)
 
 
+NUMBER_DICT = {
+    "zero": 0,
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
+
+
 class Day1:
     def __init__(self):
         self._get_total_calibration = 0
@@ -18,7 +34,7 @@ class Day1:
         self._get_total_calibration = 0
         self._current_calibration = -1
 
-    def find_calibration(self, input_string):
+    def find_calibration(self, input_string: str, advanced_calibration: bool = False):
         # Checking for invalid input types
         if type(input_string) != str:
             raise AoC2015Day1Exception(
@@ -28,14 +44,41 @@ class Day1:
         self._current_calibration = -1
 
         found_digits = []
+        found_digits_loc = []
 
-        for character in input_string:
+        for location, character in enumerate(input_string):
             try:
                 found_digits.append(int(character))
+                found_digits_loc.append(location)
             except ValueError:
                 # Ignore not a number
                 pass
 
+        found_named_digits_start = {}
+        found_named_digits_end = {}
+
+        for key in NUMBER_DICT:
+            start_loc = input_string.find(key)
+            end_loc = input_string.rfind(key)
+
+            if start_loc >= 0 and end_loc >= 0:
+                found_named_digits_start.update({key: start_loc})
+                found_named_digits_end.update({key: end_loc})
+
+        if advanced_calibration:
+            found_number = self._create_advanced_found_number(
+                found_digits,
+                found_digits_loc,
+                found_named_digits_start.copy(),
+                found_named_digits_end.copy(),
+            )
+        else:
+            found_number = self._create_found_number(found_digits)
+
+        self._update_calibration(found_number)
+
+    @staticmethod
+    def _create_found_number(found_digits: List[int]) -> int:
         if len(found_digits) > 1:
             found_number = found_digits[0] * 10 + found_digits[-1]
         elif len(found_digits) == 1:
@@ -43,13 +86,53 @@ class Day1:
         else:
             found_number = 0
 
+        return found_number
+
+    @staticmethod
+    def _create_advanced_found_number(
+        found_digits: List[int],
+        found_digits_loc: List[int],
+        found_named_digits_start: Dict[str, int],
+        found_named_digits_end: Dict[str, int],
+    ) -> int:
+        if len(found_digits) == 0:
+            first_digit_loc = -1
+            last_digit_loc = -1
+            first_digit = 0
+            last_digit = 0
+        else:
+            first_digit_loc = found_digits_loc[0]
+            last_digit_loc = found_digits_loc[-1]
+            first_digit = found_digits[0]
+            last_digit = found_digits[-1]
+
+        for found_named_digit in found_named_digits_start.keys():
+            if first_digit_loc < 0:
+                first_digit = NUMBER_DICT[found_named_digit]
+                first_digit_loc = found_named_digits_start[found_named_digit]
+            elif found_named_digits_start[found_named_digit] < first_digit_loc:
+                first_digit = NUMBER_DICT[found_named_digit]
+                first_digit_loc = found_named_digits_start[found_named_digit]
+
+            if last_digit_loc < 0:
+                last_digit = NUMBER_DICT[found_named_digit]
+                last_digit_loc = found_named_digits_end[found_named_digit]
+            elif found_named_digits_end[found_named_digit] > last_digit_loc:
+                last_digit = NUMBER_DICT[found_named_digit]
+                last_digit_loc = found_named_digits_end[found_named_digit]
+
+        found_number = first_digit * 10 + last_digit
+
+        return found_number
+
+    def _update_calibration(self, found_number: int):
         self._current_calibration = found_number
         self._get_total_calibration += self._current_calibration
 
-    def get_total_calibration(self):
+    def get_total_calibration(self) -> int:
         return self._get_total_calibration
 
-    def get_current_calibration(self):
+    def get_current_calibration(self) -> int:
         return self._current_calibration
 
 
@@ -69,4 +152,10 @@ if __name__ == "__main__":
     print(
         "Part A Sum of all of the calibration values: %d" % day1.get_total_calibration()
     )
-    # print("Part B Sum of all of the calibration values: %d" % day1.get_basement_interation())
+
+    day1.reset()
+    for calibration_input in input_data:
+        day1.find_calibration(calibration_input, True)
+    print(
+        "Part B Sum of all of the calibration values: %d" % day1.get_total_calibration()
+    )
--- test/unit_tests/python_tests/2023/year2023_day1_tests.py
+++ test/unit_tests/python_tests/2023/year2023_day1_tests.py
@@ -5,7 +5,7 @@ import unittest
 # Try to general utils code
 try:
     spec = importlib.util.spec_from_file_location(  # type: ignore
-        "mod", "../../../../python/2023/day1.py"
+        "mod", "../../../../python/2023/year2023_day1.py"
     )
     day1 = importlib.util.module_from_spec(spec)  # type: ignore
     spec.loader.exec_module(day1)
@@ -60,30 +60,54 @@ class AoC2015Day1Test(unittest.TestCase):
             self.__class__.day1.get_current_calibration(),
         )
 
-    def test_given_inputs_basement_iteration(self):
+    def test_given_inputs_advanced_calibration(self):
         self.__class__.day1 = day1.Day1()
 
-        input_strings = [")", "()())", "("]
+        input_strings = [
+            "two1nine",
+            "eightwothree",
+            "abcone2threexyz",
+            "xtwone3four",
+            "4nineeightseven2",
+            "zoneight234",
+            "7pqrstsixteen",
+            "oneonetwoonetwoone",
+        ]
 
-        expected_basement_iteration_not_found = 0
+        expected_calibration_outputs = [29, 83, 13, 24, 42, 14, 76, 11]
 
-        expected_basement_outputs = [1, 5, expected_basement_iteration_not_found]
+        expected_total_calibration = 292
 
-        for input_string, expected_basement_output in zip(
-            input_strings, expected_basement_outputs
+        expected_init_total_calibration = 0
+        expected_init_current_calibration = -1
+
+        self.assertEqual(
+            expected_init_total_calibration, self.__class__.day1.get_total_calibration()
+        )
+        self.assertEqual(
+            expected_init_current_calibration,
+            self.__class__.day1.get_current_calibration(),
+        )
+
+        for input_string, expected_calibration in zip(
+            input_strings, expected_calibration_outputs
         ):
-            self.__class__.day1.reset()
+            self.__class__.day1.find_calibration(input_string, True)
+            actual_calibration = self.__class__.day1.get_current_calibration()
+            self.assertEqual(expected_calibration, actual_calibration)
 
-            self.assertEqual(
-                expected_basement_iteration_not_found,
-                self.__class__.day1.get_basement_interation(),
-            )
+        self.assertEqual(
+            expected_total_calibration, self.__class__.day1.get_total_calibration()
+        )
 
-            self.__class__.day1.calc_floor(input_string)
-
-            actual_basement_value = self.__class__.day1.get_basement_interation()
-
-            self.assertEqual(expected_basement_output, actual_basement_value)
+        self.__class__.day1.reset()
+        self.assertEqual(
+            expected_init_total_calibration, self.__class__.day1.get_total_calibration()
+        )
+        self.assertEqual(
+            expected_init_current_calibration,
+            self.__class__.day1.get_current_calibration(),
+        )
 
     def test_bad_inputs(self):
         self.__class__.day1 = day1.Day1()
