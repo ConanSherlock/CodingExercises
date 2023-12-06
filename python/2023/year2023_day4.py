38e93627b1b82155659f55547eb81a4506897476
Conan Sherlock <conansherlock@live.ie>
2023-12-06 17:57:06 +0000

    year2023_day4.py: part b attempt 1

--- python/2023/year2023_day4.py
+++ python/2023/year2023_day4.py
@@ -25,15 +25,23 @@ class Day4:
         self._winning_numbers = []
         self._pulled_numbers = []
         self._winning_pulls = 0
+        self._winning_card = False
+        self._card_copies = {}
+        self._total_number_of_scratch_cards_won = 0
 
     def reset(self):
         self._card_value = 0
         self._sum_card_values = 0
         self._winning_numbers.clear()
         self._pulled_numbers.clear()
+        self._card_copies.clear()
         self._winning_pulls = 0
+        self._winning_card = False
+        self._total_number_of_scratch_cards_won = 0
 
     def calc_scratch_card_value(self, card: str):
+        self._winning_card = False
+
         """Search the schematic for part numbers."""
         if not isinstance(card, str):
             raise AoC2023Day4Exception(f"{self.INVALID_TYPE_SELECTION}: {type(card)}")
@@ -56,6 +64,25 @@ class Day4:
         self._count_winning_pulls()
         self._update_sum_cards_value()
 
+    def calc_total_cards(self, card_list: List[str]):
+        self._card_copies = {i: 0 for i in range(len(card_list))}
+        for card_num, card in enumerate(card_list):
+            self.calc_scratch_card_value(card)
+            self._update_card_copy_dict(card_num)
+
+        for card_num in self._card_copies:
+            for i in range(0, self._card_copies[card_num]):
+                self.calc_scratch_card_value(card_list[card_num])
+                self._update_card_copy_dict(card_num)
+            self._total_number_of_scratch_cards_won += self._card_copies[card_num]
+
+    def _update_card_copy_dict(self, card_num):
+        if self._winning_card:
+            for i in range(1, self._winning_pulls + 1):
+                card_copy_key = card_num + i
+                if card_copy_key in self._card_copies:
+                    self._card_copies[card_copy_key] += 1
+
     def _count_winning_pulls(self):
         for pulled_number in self._pulled_numbers:
             if pulled_number in self._winning_numbers:
@@ -64,6 +91,7 @@ class Day4:
     def _update_sum_cards_value(self):
         if self._winning_pulls > 0:
             self._card_value = self.SCORE_MULTIPLIER ** (self._winning_pulls - 1)
+            self._winning_card = True
         else:
             self._card_value = 0
         self._sum_card_values += self._card_value
@@ -79,21 +107,34 @@ class Day4:
     def get_total_card_value(self) -> int:
         return self._sum_card_values
 
+    def get_total_cards_won(self) -> int:
+        return self._total_number_of_scratch_cards_won
+
 
 if __name__ == "__main__":
     day4_file_path = os.path.abspath("../../test/input_data/2023/day4_input.txt")
     if not os.path.exists(day4_file_path):
         raise FileNotFoundError(f"File not found: {day4_file_path}")
     day4 = Day4()
+    # try:
+    #     with open(day4_file_path, "r") as input_file:
+    #         input_scratch_cards = input_file.readlines()
+    #         if not input_scratch_cards:
+    #             raise AoC2023Day4Exception("Input schematic is empty")
+    #         for current_card in input_scratch_cards:
+    #             day4.calc_scratch_card_value(current_card)
+    # except OSError as e:
+    #     raise AoC2023Day4Exception(f"Error reading file: {e}")
+    # print("--- Day 4: Scratchcards ---")
+    # print(f"Part A Point value of all cards: {day4.get_total_card_value()}")
+
+    day4.reset()
     try:
         with open(day4_file_path, "r") as input_file:
             input_scratch_cards = input_file.readlines()
             if not input_scratch_cards:
                 raise AoC2023Day4Exception("Input schematic is empty")
-            for current_card in input_scratch_cards:
-                day4.calc_scratch_card_value(current_card)
+            day4.calc_total_cards(input_scratch_cards)
     except OSError as e:
         raise AoC2023Day4Exception(f"Error reading file: {e}")
-    print("--- Day 4: Scratchcards ---")
-    print(f"Part A Point value of all cards: {day4.get_total_card_value()}")
-    # print(f"Part B Sum of all of the gear ratio values: {day4.get_sum_gear_ratio()}")
+    print(f"Part B Sum of all of the gear ratio values: {day4.get_total_cards_won()}")
